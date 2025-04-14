edc288403b75f48585fdcc683465c20aee97c638
Apenas-eu007 <gitxhub00@tuta.io>
2025-04-14 18:21:12 +0000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function changeTitleColor() {
+    document.getElementById('main-title').style.color = 'purple';
+}
+
+function resetTitleColor() {
+    document.getElementById('main-title').style.color = '#ff6347';
+}
